291f7600347ced1841204a385b76fff7bc6b38c6
jimmy <m17816612557@163.com>
2021-09-15 01:00:45 +0800

    update:loyout

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,50 @@
 <template>
   <div id="app">
-    lzm
+    <el-row>
+      <el-button>默认按钮</el-button>
+      <el-button type="primary">主要按钮</el-button>
+      <el-button type="success">成功按钮</el-button>
+      <el-button type="info">信息按钮</el-button>
+      <el-button type="warning">警告按钮</el-button>
+      <el-button type="danger">危险按钮</el-button>
+    </el-row>
+    <button class="btn">123</button>
     <router-view/>
   </div>
 </template>
 
-<style lang="scss" scoped></style>
+<script lang="ts">
+import Vue from 'vue'
+// import request from '@/utils/request'
+
+// console.log(request)
+
+// request({
+//   method: 'GET',
+//   url: '/boss'
+// }).then(res => {
+//   console.log(res)
+// })
+
+export default Vue.extend({
+  data () {
+    return {
+      a: 1
+    }
+  },
+
+  methods: {
+    test () {
+      this.a = 2
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+// @import '~@/styles/variables.scss';
+.btn {
+  background-color: $info-color;
+}
+
+</style>
